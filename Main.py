f57d1e9f54c62698559ae54a48ab97ce9e3e678a
Medina <medina.c@rogers.com>
2021-02-27 20:31:48 -0500

    Get data from initial search result

--- Main.py
+++ Main.py
@@ -6,23 +6,45 @@ scopes = "playlist-read-private playlist-read-collaborative"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scopes))
 
+# TO INCLUDE: 
+#album, main artist, all artists, duration, name, popularity, item number
+#end time, end date, ms listened
+#danceability, energy, key?, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, time signature, mode (major or minor), 
+
 def main():
     print("Enter file path for streaming history JSON file: ")
+    baseData = getBaseData()
+    baseData = [baseData[0]]
+    data = {}
 
-    data = getBaseData()
+    trackIds = [] # List to later group search the audio features
 
-    track = data[0]
+    # Search each item and save useful data
+    for i, item in enumerate(baseData):
     
-    query = 'track:"{name}" artist:"{artist}"'.format(
-        name = track['trackName'], artist = track['artistName'])
-    
-    result = sp.search(q=query, market = 'CA')
+        query = 'track:"{name}" artist:"{artist}"'.format(
+            name = item['trackName'], artist = item['artistName'])
+        
+        result = sp.search(q=query, market = 'CA', type = 'track')
 
-    trackId = result['tracks']['items'][0]['id']
-    
-    #album, artists, duration, name, popularity, track number
-    #danceability, energy, key?, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, time signature, mode (major or minor), 
+        track = result['tracks']['items'][0] # Assume the first result is the right one
 
+        trackIds.append(track['id'])
+
+        data[i] = {}
+        data[i]['Title'] = track['name']
+        data[i]['Time Listened'] = item['msPlayed']
+        data[i]['Listen Date'] = item['endTime'].split(' ')[0]
+        data[i]['End Time'] = item['endTime'].split(' ')[1]
+        data[i]['Principal Artist'] = track['artists'][0]['name']
+        data[i]['All Artists'] = getArtists(track)
+        data[i]['Album'] = track['album']['name']
+        data[i]['Track Number'] = track['track_number']
+        data[i]['Length'] = track['duration_ms']
+        data[i]['Popularity'] = track['popularity']
+   
+
+# Get the data from the original Streaming History json file provided
 def getBaseData():
     
     filePath = input()
@@ -39,6 +61,13 @@ def getBaseData():
     except:
         print('File not found. Try again or hit enter to cancel.')
         return getBaseData()
+
+# Get all artists for a track
+def getArtists(track):
+    artists = []
+    for artist in track['artists']:
+        artists.append(artist['name'])
+    return artists
 
 
 main()
